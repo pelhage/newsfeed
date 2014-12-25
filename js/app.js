7c4d861bbf1002166f168631fe7423c783148d05
Patrick <patrick425@gmail.com>
2014-12-25 11:39:50 -0500

    Deleted old files from previous workflow

--- js/app.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    
-    var app = angular.module('contentApp',['ngAnimate']);
-
-}());
-
-
-// http://stackoverflow.com/questions/2090551/parse-query-string-in-javascript http://stackoverflow.com/questions/1714786/querystring-encoding-of-a-javascript-object
-
